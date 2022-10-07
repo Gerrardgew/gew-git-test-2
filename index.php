74b5bfa14404fdcb0a31e5d80adf3d90eb85e1aa
Steven Gerrard <gerrardgew3@gmail.com>
2022-10-07 13:31:07 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
   <h1>Hello, World!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis totam temporibus nihil odio aliquid nemo, aut dignissimos, et magni quod porro eos sequi? Omnis inventore atque fugiat, molestias molestiae reiciendis!</p>
 </body>
 </html>
